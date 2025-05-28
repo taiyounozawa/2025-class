f4ab2a559b90300324b07c3e87a6c6f48ebc6d77
Taiyou Nozawa <f080vgw@yamaguchi-u.ac.jp>
2025-05-28 16:04:50 +0900

    丸の大きさ，文字の大きさを変更

--- click-number/js/scripts.js
+++ click-number/js/scripts.js
@@ -5,13 +5,22 @@ for (var num=9;num>0;num--) {
 	elm.setAttribute("class", "circle"); 
 	var function_name = "remove(" + num + ")" 
 	elm.setAttribute("onclick", function_name);
+
+    // 大きさをランダム
+    var size = 25 + Math.floor(Math.random() * 100); 
+    elm.style.width = size + "px";
+    elm.style.height = size + "px";
+
+    elm.style.fontSize = (size * 0.8) + "px";
+
+
 	document.getElementById("main").appendChild(elm);
 
 	var left_pos = 10;
 	var top_pos = 100;
 
-	left_pos = left_pos + Math.floor(Math.random() * 400);
-	top_pos = top_pos + Math.floor(Math.random() * 600);
+	left_pos = left_pos + Math.floor(Math.random() * 1000);
+	top_pos = top_pos + Math.floor(Math.random() * 400);
 
 	document.getElementById(num).style.left = "" + left_pos + "px" ;
 	document.getElementById(num).style.top = "" + top_pos + "px" ;
